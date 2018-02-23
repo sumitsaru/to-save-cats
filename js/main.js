052d28bcd014702ae8ee49e980da8a653c01f8f6
Matt <mtounkara90@gmail.com>
2018-02-23 15:23:07 -0500

    Added comments to main index.html, main.css, index.html for contact us, main.css for contact us and main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+/*
+Name: Matt Tounkara
+Date: 02/22/2018
+Description: This is the main javascript file for the to-save-cats homework.
+The purpose of the homework was to implement a donation website to raise funds
+for the sake of the world's cats. My teammates and I implemented four pages within
+our website: the main page index.html; the about us page; the contact us page and
+the donations page. This javascript file will be in charge of validating the contact
+us form's data entry by the user, before being submitted to the server.
+*/
+
+
 // function toValidate(){
 //   var fName = document.forms["theForm"]["firstName"].value;
 //   var theAN = /^[\w ] + $/;
